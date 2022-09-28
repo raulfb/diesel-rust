af3e4edf2e53b2bf67ac0929e0f40b9a7501f99b
Raul Franco <raul.franco@norvoz.es>
2022-09-28 09:31:44 +0200

    get usuario

--- src/consulta.rs
+++ src/consulta.rs
@@ -5,8 +5,11 @@ use self::rust_diesel::*;
 use self::models::*;
 use self::diesel::prelude::*;
 fn main(){
-    get_usuarios(); 
+    // get_usuarios(); 
+    let id_usuario=10;
+    get_usuario(id_usuario);
 }
+
 
 fn get_usuarios(){
     let connection = &mut establish_connection();
@@ -14,10 +17,8 @@ fn get_usuarios(){
     let results = usuarios       
         .limit(5)
         .load::<Usuarios>(connection)
-        .expect("Error loading posts");
-    println!("-----------------------------------------");
-    println!("Se encontraron {} usuarios  ", results.len());
-    println!("----------------------------------------\n");
+        .expect("Error obteniendo los usuarios");
+
     for usuario in results{
         let estado_usuario;
         if usuario.activo == true{
@@ -29,6 +30,24 @@ fn get_usuarios(){
     }
 }
 
-// fn get_usuario(){
-
-// }
+fn get_usuario(id_usuario:i32){
+    let connection = &mut establish_connection();
+    use self::schema::usuarios::dsl::*;
+    let results = usuarios
+        .filter(id.eq(id_usuario))       
+        .limit(5)
+        .load::<Usuarios>(connection)
+        .expect("Error obteniendo el usuario");
+        // println!("{:?}",results);
+        if results.len()>0{
+            let estado_usuario;
+            if results[0].activo == true{
+                estado_usuario="Activo".to_string();
+            }else{
+                estado_usuario="Inactivo".to_string();
+            }
+           println!("Nombre usuario: {}, apellidos: {} , estado {} ",results[0].nombre,results[0].apellidos,estado_usuario);
+        }else{
+            println!("No existe el usuario con id {}",id_usuario);
+        }
+}
